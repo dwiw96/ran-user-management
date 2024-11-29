fea5abb58a8bf67967b2e2ceb863ef208667aaae
dwiw96 <dwiwahyudi1996@gmail.com>
2024-11-29 10:54:59 +0700

    create: postgres connection

---
+++ pkg/driver/postgresql/postgresql.go
@@ -0,0 +1,28 @@
+package postgresql
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	config "github.com/dwiw96/ran-user-management/config"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func ConnectToPg(envCfg *config.EnvConfig) *pgxpool.Pool {
+	pgAddress := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", envCfg.DB_USERNAME, envCfg.DB_PASSWORD, envCfg.DB_HOST, envCfg.DB_PORT, envCfg.DB_NAME)
+	configPgAddress, err := pgxpool.ParseConfig(pgAddress)
+	if err != nil {
+		log.Fatal("cannot parse config, msg:", err)
+	}
+
+	dbPool, err := pgxpool.New(context.Background(), configPgAddress.ConnString())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+
+	return dbPool
+}
